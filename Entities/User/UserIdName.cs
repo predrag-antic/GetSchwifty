9e94528d9f4b1039f63f6a8cf931f5c7df225a92
Milos Veljkovic <veljkovicmilos559@gmail.com>
2020-01-02 15:43:00 +0100

    Add Favorite Place/Band

---
+++ Entities/User/UserIdName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GetSchwifty.Entities
+{
+    public class UserIdName
+    {
+        public string userId { get; set; }
+        public string name { get; set; } //name of place or band, that uniqe(like id) for band/place
+    }
+}
